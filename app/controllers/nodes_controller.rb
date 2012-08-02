880422e9b0abc9f50300192d9b8b7febdac218b9
Andy Yao <andyaoy@gmail.com>
2012-08-03 06:31:59 +0800

    Implement edit for node

--- app/controllers/nodes_controller.rb
+++ app/controllers/nodes_controller.rb
@@ -19,6 +19,22 @@ class NodesController < ApplicationController
           format.html { render :action => :new }
       end
     end
+  end
 
+  def edit
+    @node  = Node.find(params[:id])
+    @categories = Category.all
+  end
+
+  def update
+    @node = Node.find(params[:id])
+
+    respond_to do |format|
+      if @node.update_attributes(params[:node])
+          format.html { redirect_to(@node, :notice => 'Node was successfully updated.') }
+      else
+          format.html { render :action => :new }
+      end
+    end
   end
 end
